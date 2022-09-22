8cdec89fd5f37374d56e97563c3a3d7a341697d4
Kamil Jarczak <kamilj12@gmail.com>
2022-09-22 23:39:13 +0200

    add SingleMeal.vue and MealElement.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">Hello word</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+name: "myHeader",
+data(){
+  return 0
+},
+meals:[],
+mealElements:[]
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.wrapper{
+  width: 100%;
+  margin: 0;
+  padding: 30px;
 }
 </style>
